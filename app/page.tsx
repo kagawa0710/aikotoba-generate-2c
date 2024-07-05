e8d631c280541f0b9c8e8b7b38dfed6197243c47
kagawa0710 <58012645+kagawa0710@users.noreply.github.com>
2024-07-05 21:45:35 +0900

    create generater

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,273 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+
+const Aikotobar = () => {
+  const firstWords = [
+    "夜の",
+    "ただただ",
+    "終わりを告げる",
+    "秋を彩る",
+    "声が大きい",
+    "先のとがった",
+    "不老不死の",
+    "黒き疾風の",
+    "こちらを見る",
+    "大昔の",
+    "バズった",
+    "チリとなった",
+    "空前絶後の",
+    "難しく考えすぎる",
+    "なぜか",
+    "持ち手のない",
+    "悟りを開く",
+    "暗闇に包まれた",
+    "絶体絶命の",
+    "嬉しそうな",
+    "投げられた",
+    "ぶん投げられた",
+    "最初で最後の",
+    "最小の",
+    "常識外れの",
+    "世界に一つだけの",
+    "奥が深い",
+    "変に",
+    "助けを求める",
+    "虹色の",
+    "夢も希望もない",
+    "天国のような",
+    "素晴らしい",
+    "初心者向けの",
+    "何とか生き延びた",
+    "善い行いをした",
+    "親孝行をした",
+    "親孝行をしなかった",
+    "先日逮捕された",
+    "思い出し笑いをした",
+    "注目を集めた",
+    "世間から叩かれた",
+    "アンチコメントだらけの",
+    "二度と消えない",
+    "時間を無駄にする",
+    "金儲けが好きな",
+    "世界のための",
+    "そこそこ",
+    "最近",
+    "管理者である",
+  ];
+  const secondWords = [
+    "最強の",
+    "惨めな",
+    "考える",
+    "始まりの",
+    "最大の",
+    "美しい",
+    "白い",
+    "とても小さな",
+    "灰色の",
+    "剝がれた",
+    "盗られた",
+    "幼い",
+    "悪賢い",
+    "ゴム製の",
+    "最高な",
+    "汚れた",
+    "縮んだ",
+    "曲がった",
+    "おかしい",
+    "悟りを開く",
+    "折れた",
+    "ヤバい",
+    "勇者の",
+    "巨大な",
+    "挟まれた",
+    "抜け出せない",
+    "六千円する",
+    "永遠の",
+    "数学好きな",
+    "百年後の",
+    "薄い",
+    "緑色の",
+    "瑠璃色の",
+    "謎の",
+    "うるさい",
+    "かわいそうな",
+    "笑顔の",
+    "怒った",
+    "悲しみの",
+    "おもしろい",
+    "面白おかしい",
+    "やらかした",
+    "有名な",
+    "無名な",
+    "流行りの",
+    "弱い",
+    "残酷な",
+    "残念な",
+    "すごい",
+    "偉い",
+  ];
+  const thirdWords = [
+    "不死鳥",
+    "紅葉",
+    "親友",
+    "ウグイス",
+    "防寒着",
+    "葉脈",
+    "万年筆",
+    "羊皮紙",
+    "石ころ",
+    "世界",
+    "ポスター",
+    "財布",
+    "幼稚園児",
+    "大人",
+    "木",
+    "クジャク",
+    "退会ボタン",
+    "ピンポン玉",
+    "宇宙",
+    "ロケット",
+    "神",
+    "カッターナイフ",
+    "映画",
+    "米粒",
+    "話",
+    "サル",
+    "ゾウ",
+    "レモン",
+    "ペット",
+    "惑星",
+    "定規",
+    "英雄",
+    "木箱",
+    "鉄橋",
+    "目覚まし時計",
+    "学校",
+    "病院",
+    "カレンダー",
+    "鉛筆削り器",
+    "スプーン",
+    "ハンドル",
+    "ゴルフボール",
+    "板",
+    "浮き輪",
+    "ポット",
+    "蛍光灯",
+    "茶柱",
+    "岩",
+    "剣",
+    "孫の手",
+    "生卵",
+    "ぬいぐるみ",
+    "ギャグ",
+    "マウスポインタ",
+    "椅子",
+    "カーテン",
+    "コンサート",
+    "アプリ",
+    "スマホ",
+    "時計",
+    "納税者",
+    "立方体",
+    "直角三角形",
+    "レーザービーム",
+    "珊瑚礁",
+    "つばめ",
+    "スズメ",
+    "カモメ",
+    "広告",
+    "冠",
+  ];
+  const fourthWords = [
+    "とその真実",
+    "に隠された秘密",
+    "の誕生",
+    "の滅亡",
+    "の結末",
+    "の五秒後",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ];
+  const fifthWords = ["を見てしまった", "", "", "", "", ""];
+
+  const [result, setResult] = useState("");
+  const [kinds, setKinds] = useState(0);
+
+  const arrayRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+  const generateResult = () => {
+    setResult(
+      arrayRandom(firstWords) +
+        arrayRandom(secondWords) +
+        arrayRandom(thirdWords) +
+        arrayRandom(fourthWords) +
+        arrayRandom(fifthWords)
+    );
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(result);
+    document.getElementById("copy").innerText = "コピーしました！";
+    setTimeout(
+      () => (document.getElementById("copy").innerText = "コピー"),
+      1000
+    );
+  };
+
+  useEffect(() => {
+    setKinds(
+      firstWords.length *
+        secondWords.length *
+        thirdWords.length *
+        (fourthWords.length - 8) *
+        (fifthWords.length - 4)
+    );
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-black">
+      <h1 className="text-4xl font-bold mb-4">Aikotobar</h1>
+      <p className="text-lg mb-4">合言葉を自動生成してくれるツールです</p>
+      <p className="text-lg mb-4">
+        Aikotobarでは{" "}
+        <span id="kinds" className="font-bold">
+          {kinds}
+        </span>{" "}
+        通りの合言葉を作成できます
+      </p>
+      <p className="mb-4">
+        <button
+          id="btn"
+          onClick={generateResult}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          作成
+        </button>
+      </p>
+      <div id="result-div" className="mb-4 p-4 bg-white rounded shadow-lg">
+        <p id="result" className="text-xl">
+          {result ? result : "ここに合言葉が生成されます"}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <p>
+        <button
+          id="copy"
+          onClick={copyToClipboard}
+          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          コピー
+        </button>
+      </p>
+    </div>
   );
-}
+};
+
+export default Aikotobar;
